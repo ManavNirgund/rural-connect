98be4232b701ce86f8dc486ce060926e1327d60a
snehaapramod <snehaapramod@gmail.com>
2023-07-18 21:05:41 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
 
       <Routes>
         <Route exact path="/" element={<Login />}></Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-import { MDBNavbar, MDBContainer, MDBNavbarBrand, MDBNavbarNav, MDBNavbarItem, MDBNavbarLink, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink } from 'mdb-react-ui-kit';
+import React from "react";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Link } from "react-router-dom";
+import ruralconnect from "../../assets/images/logo/svg/RuralConnect.svg"
 
-const Header = () => {
+const NavBar = () => {
   const [showNav, setShowNav] = React.useState(false);
 
   const toggleNav = () => {
@@ -9,30 +23,40 @@ const Header = () => {
   };
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
-      <MDBContainer>
-        <MDBNavbarBrand href='/'>Navbar Brand</MDBNavbarBrand>
+    <MDBNavbar expand="lg" light style={{ backgroundColor: "#75B59E" }}>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">
+          <img src={ruralconnect} width="100vw" style={{marginBottom: "-0.5rem"}}/>
+        </MDBNavbarBrand>
         <MDBNavbarToggler onClick={toggleNav} />
-
         <MDBCollapse navbar show={showNav}>
-          <MDBNavbarNav className='mr-auto'>
+          <MDBNavbarNav className="mr-auto">
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Products</MDBNavbarLink>
+              <MDBNavbarLink href="#" style={{color: "black"}}>Home</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#" style={{color: "black"}}>Weather</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#" style={{color: "black"}}>News</MDBNavbarLink>
             </MDBNavbarItem>
           </MDBNavbarNav>
-
-          <MDBNavbarNav right>
+          <MDBNavbarNav className="ml-auto">
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Login</MDBNavbarLink>
+              <MDBBtn color="secondary" className="me-4" style={{boxShadow: 'none'}}>
+                <Link to="/">Login</Link>
+              </MDBBtn>
             </MDBNavbarItem>
             <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Logout</MDBNavbarLink>
+              <MDBBtn color="secondary" style={{boxShadow: 'none'}}>
+                <Link to="/register">Register</Link>
+              </MDBBtn>
             </MDBNavbarItem>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
     </MDBNavbar>
   );
-}
+};
 
-export default Header;
+export default NavBar;
